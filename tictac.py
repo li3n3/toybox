c2975f64a77389c72ed30044bdf05595445f3618
Liene Verzemnieks <liene@simple.com>
2015-05-25 17:34:02 -0700

    tic tac toe works yayyyy

--- tictac.py
+++ tictac.py
@@ -1,15 +1,3 @@
-# test values to print
-# a1 = "X"
-# b1 = "X"
-# c1 = "O"
-
-# a2 = "O"
-# b2 = "O"
-# c2 = "X"
-
-# a3 = "X"
-# b3 = "O"
-# c3 = "X"
 
 def showboard():
     # prints out a sweet board where each box is named with a number/letter
@@ -20,8 +8,80 @@ def showboard():
     print "  - * - * -"
     print "3 {a3} | {b3} | {c3}".format(**space_dict)
 
-# showboard()
+space_dict = {"a1": " ", "b1": " ", "c1": " ",
+              "a2": " ", "b2": " ", "c2": " ",
+              "a3": " ", "b3": " ", "c3": " "}
 
+def tictactime():
+# we start with a blank board
+    showboard()
+    # we'll get the winner back from playturn:
+    winner = playturn('X')
+    if winner == 'X' or winner == 'O':
+        print "Nice playing! Looks like the winner was " + str(winner) + "."
+    else:
+        print "Bummer. Looks like the winner was " + str(winner)
+
+def winning():
+    winning_conditions = [space_dict["a1"] == space_dict["b1"] == space_dict["c1"]
+                          and space_dict["a1"] != " ",
+                          space_dict["a2"] == space_dict["b2"] == space_dict["c2"]
+                          and space_dict["a2"] != " ",
+                          space_dict["a3"] == space_dict["b3"] == space_dict["c3"]
+                          and space_dict["a3"] != " ",
+                          space_dict["a1"] == space_dict["a2"] == space_dict["a3"]
+                          and space_dict["a1"] != " ",
+                          space_dict["b1"] == space_dict["b2"] == space_dict["b3"]
+                          and space_dict["b1"] != " ",
+                          space_dict["c1"] == space_dict["c2"] == space_dict["c3"]
+                          and space_dict["c1"] != " ",
+                          space_dict["a1"] == space_dict["b2"] == space_dict["c3"]
+                          and space_dict["a1"] != " ",
+                          space_dict["c1"] == space_dict["b2"] == space_dict["a3"]
+                          and space_dict["c1"] != " "]
+    if True in winning_conditions:
+        return True
+    else:
+        return False
+
+
+def playturn(player):
+    turn = raw_input("What space would you like to play, {0}?\n".format(player))
+    # just in case it's caps
+    turn = turn.lower()
+    # We expect something like a2, b1, c3
+    # either it's a bad input (not a board position)
+    # and we ask for something else
+    if turn not in space_dict:
+        print "Hmm, that's not a board space. Try something like a2 or c3."
+        print
+        return playturn(player)
+    # or it's a good input, but the space is already played, and we ask for
+    # something else
+    elif space_dict[turn] == 'X' or space_dict[turn] == 'O':
+        print "That spot's already played. Try another!"
+        print
+        return playturn(player)
+    # or it's a legit good input, and we assign that value in space_dict
+    else:
+        space_dict[turn] = player
+        # and we print that on the board
+        showboard()
+        # and we don't keep playing if winning happened
+        if winning():
+            return player
+        # or if there was a tie and the board is full
+        elif ' ' not in space_dict.values():
+            # print "Aw, that's a tie. Nice playing!"
+            return "no one.  :("
+        else:
+            if player == 'X':
+                return playturn('O')
+            else:
+                return playturn('X')
+
+
+# Let's start this party!
 print "Hey there! Welcome to tic-tac-toe. I'll be your robot host today."
 print "Grab a friend, or play against yourself. (I'm working on my skills.)"
 print "Each spot is named by a number and a letter, like this:"
@@ -52,78 +112,6 @@ if ready == "n":
     print "No problem. Grab me a byte to eat and come back later?"
 elif ready == "y":
     print "Oh good; me too."
+    tictactime()
 else:
     print "Hmm. That didn't look like either y or n."
-
-# Our starting board:
-# a1, b1, c1 = " ", " ", " "
-# a2, b2, c2 = " ", " ", " "
-# a3, b3, c3 = " ", " ", " "
-
-space_dict = {"a1": " ", "b1": " ", "c1": " ",
-              "a2": " ", "b2": " ", "c2": " ",
-              "a3": " ", "b3": " ", "c3": " "}
-
-# spaces = ["a1", "b1", "c1", "a2", "b2", "c2", "a3", "b3", "c3"]
-# played_spaces = ['a2', 'b2']
-    
-# winning_conditions = [a1 == b1 == c1 and a1 != ' ', a2 == b2 == c2 and a2 != ' ',
-#                       a3 == b3 == c3 and a3 != ' ', a1 == a2 == a3 and a1 != ' ',
-#                       b1 == b2 == b3 and b1 != ' ', c1 == c2 == c3 and c1 != ' ',
-#                       a1 == b2 == c3 and a1 != ' ', c1 == b2 == a3 and c1 != ' ']
-
-winning_conditions = [space_dict["a1"] == space_dict["b1"] == space_dict["c1"]
-                      and space_dict["a1"] != " "]
-
-# the_game
-def tictactime():
-# we start with a blank board
-    showboard()
-
-    if True in winning_conditions:
-        print "Someone won!"
-        print "And that someone was ... the winner!"
-    if length(played_spots) == 9:
-        return "Aw, that's a tie. Nice playing!"
-
-
-def playturn(player):
-# X plays a turn
-    turn = raw_input("What space would you like to play, {0}?\n".format(player))
-    # just in case it's caps
-    turn = turn.lower()
-    # We expect something like a2, b1, c3
-    # either it's a bad input (not a board position, or already taken)
-    # and we ask for something else
-    if turn not in spaces:
-        print "Hmm, that's not a board space. Try something like a2 or c3."
-        print
-        playturn(player)
-    # or it's a good input, but the space is already played
-    elif turn in played_spaces:
-        print "That spot's already played. Try another!"
-        print
-        playturn(player)
-    # or it's a legit good input, and we add it to the played spaces
-    else:
-        played_spaces.append(turn)
-        # and we print that on the board
-        print "turn is " + turn
-        turn = player
-        print "turn is " + turn
-
-playturn("X")
-
-
-
-
-# then O plays a turn
-#     same stuff as above
-
-# this continues until one of the 8 winning conditions is met
-# OR until all positions are filled
-
-# then it tells us who won 
-# or that no one won 
-# and then we're done (maybe it asks us to play again?)
-
