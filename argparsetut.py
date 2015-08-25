b8b0a09778d60673b61538073cf574f8585d44f2
Liene Verzemnieks <liene@simple.com>
2015-08-25 17:33:43 -0400

    restrict choices for verbosity, a lil breathing room

--- argparsetut.py
+++ argparsetut.py
@@ -5,10 +5,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("square", type = int,
                     help = "display a square of a given number")
 parser.add_argument("-v", "--verbosity", help = "increase output verbosity",
-                    type = int)
+                    type = int, choices = [0, 1, 2])
 
 args = parser.parse_args()
 answer = args.square**2
+
 if args.verbosity == 2:
     print "the square of {} equals {}".format(args.square, answer)
 elif args.verbosity == 1:
