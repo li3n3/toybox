d842dec2c008b2f89e150139eb43031753f0052c
Liene Verzemnieks <liene@simple.com>
2015-08-25 17:39:01 -0400

    rebelliously changing only the problem line

--- argparsetut.py
+++ argparsetut.py
@@ -9,7 +9,8 @@ parser.add_argument("-v", "--verbosity", help = "increase output verbosity", act
 args = parser.parse_args()
 answer = args.square**2
 
-if args.verbosity == 2:
+# catch a case: replace == with >=
+if args.verbosity >= 2:
     print "the square of {} equals {}".format(args.square, answer)
 elif args.verbosity == 1:
     print "{}^2 == {}".format(args.square, answer)
