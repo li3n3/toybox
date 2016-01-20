a8f80250cd032316b9ff9b1a3db7974ff8f15cd1
Liene Verzemnieks <liene.v@gmail.com>
2016-01-19 16:51:46 -0800

    okay so this only rounds up, not down

--- trashscript.py
+++ trashscript.py
@@ -1,9 +1,20 @@
-def near_ten(num):
-    if (num % 10) > 2 and (num % 10) < 8:
-        print "num % 10 is {0}".format(num % 10)
-        # meow
-        return False
-    else:
-        return True
+""" For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "def round10(num):" and call it 3 times. Write the helper entirely below and at the same indent level as round_sum().
+"""
 
-print near_ten(12)
+def round_sum(a, b, c):
+    return round10(a) + round10(b) + round10(c)
+
+def round10(num):
+    if str(num)[-1] < 5:
+        remove = num % 5
+        num -= remove
+        print "I removed {} from the original num nums.".format(remove)
+    else:
+        if str(num)[-1] == 5:
+            add = 5
+        else:
+            add = 5 - (num % 5)
+        num += add
+        print "I added {} to the original num nums.".format(add)
+
+    return num
