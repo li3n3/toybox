89857b648d1938fd6199f16b0a0ac538337ec43d
Liene Verzemnieks <liene.v@gmail.com>
2016-01-20 11:48:23 -0800

    string play with codingbat

--- trashscript.py
+++ trashscript.py
@@ -1,29 +1,23 @@
+""" Return the number of times that the string "code" appears anywhere in the
+given string, except we'll accept any letter for the 'd', so "cope" and "cooe"
+count.
 """
-Given three ints, a b c, return True if one of b or c is "close" (differing from
-a by at most 1), while the other is "far", differing from both other values by 2
-or more. Note: abs(num) computes the absolute value of a number.
-"""
 
-def close_far(a, b, c):
-    if abs(b - a) < 2:
-        bclose = True
-    else:
-        bclose = False
-    if abs(b - a) >= 2 and abs(b - c) >= 2:
-        bfar = True
-    else:
-        bfar = False
+def count_code(str):
+    codecount = 0
+    for num in range(0, len(str) - 3):
+        print "okay this time num is {}".format(num)
+        # look for a c at that index
+        if str[num] == 'c':
+        # if you find it, look for an o
+            if str[num + 1] == 'o':
+        # the next letter is fine, no matter what
+        # so conveniently we don't have to do anything
+        # look for an e
+                if str[num + 3] == 'e':
+        # if you got this far, increment counter by 1
+                    codecount += 1
+        # and repeat!
 
-    if abs(c - a) < 2:
-        cclose = True
-    else:
-        cclose = False
-    if abs(c - a) >= 2 and abs(c - b) >=2:
-        cfar = True
-    else:
-        cfar = False
-
-    if (bclose == True and cfar == True) or (bfar == True and cclose == True):
-        return True
-    else:
-        return False
+        # return codecount
+    return codecount
