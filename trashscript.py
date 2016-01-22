adc413a92048a2f9ea422fe540721c25bf94e06e
Liene Verzemnieks <liene.v@gmail.com>
2016-01-22 09:48:37 -0800

    redo earlier thing for practice

--- trashscript.py
+++ trashscript.py
@@ -1,6 +1,14 @@
-"""Return True if the given string contains an appearance of "xyz" where the xyz
-is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+""" Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
 """
 
-def xyz_there(str):
-
+def pos_neg(a, b, negative):
+    if negative:
+        if a < 0 and b < 0:
+            return True
+        else:
+            return False
+    else:
+        if (a < 0 and b > 0) or (b < 0 and a > 0):
+            return True
+        else:
+            return False
