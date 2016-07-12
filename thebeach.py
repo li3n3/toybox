197143ccd09fb152b22a02fd4ee0dd0d5fa71269
Liene Verzemnieks <liene.v@gmail.com>
2016-07-12 11:30:06 -0700

    all these print statements found the unnecessary break

--- thebeach.py
+++ thebeach.py
@@ -5,18 +5,22 @@
 def sum67(nums):
     sum = 0
     i = 0
-    for i in xrange(len(nums)):
-        if nums[i] == 6:
+    while i < len(nums):
+        if nums[i] != 6:
+            sum += nums[i]
+            i += 1
+            print "I added the number then incremented i"
+        else:
             # don't add anything to sum
             # do increment the iterator
             i += 1
+            print "I incremented a thing because the number was 6"
             # check if the next number is seven
             # keep checking until the next number is 7
             while nums[i] != 7:
                 i += 1
-            # then break out of this loop
+                print "it's still not 7; I incremented the counter"
+            # then increment i to get past the 7, and break out of this loop
             i += 1
-            break
-        else:
-            sum += nums[i]
+            print "I incremented the counter and now we'll break"
     return sum
